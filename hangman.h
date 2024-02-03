5c493fa49bf51865c7e5dcb2230efa2b4f1ff8dd
[allanprado] <[allanprado.it@gmail.com]>
2024-02-03 16:19:11 -0300

    add header file

---
+++ hangman.h
@@ -0,0 +1,17 @@
+#ifndef HANGMAN_H
+# define HANGMAN_H
+
+# include "./libftsrc/libft.h"
+# include <stdlib.h>
+# include <stdio.h>
+
+struct player
+{
+	char *name;
+	int lifes;
+	char guess;
+	char *word;
+	char *hidenword;
+};
+
+#endif
